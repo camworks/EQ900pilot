d1e039d5ad21d8780d84b41f21d92a6756cb9166
neokii <neokii@gmail.com>
2022-02-16 19:48:03 +0900

    screen recoder for EON

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -6,10 +6,8 @@
 #include "selfdrive/ui/qt/widgets/cameraview.h"
 #include "selfdrive/ui/ui.h"
 
-#ifdef QCOM2
 #include <QTimer>
 #include "selfdrive/ui/qt/screenrecorder/screenrecorder.h"
-#endif
 
 
 // ***** onroad widgets *****
@@ -101,12 +99,10 @@ private:
   QHBoxLayout* split;
 
   // neokii
-#ifdef QCOM2
 private:
   ScreenRecoder* recorder;
   std::shared_ptr<QTimer> record_timer;
   QPoint startPos;
-#endif
 
 private slots:
   void offroadTransition(bool offroad);
