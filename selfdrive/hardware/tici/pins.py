4df04475ebf713c24139a36f293815daccfbf889
boxkon <70835190+boxkon@users.noreply.github.com>
2022-02-16 13:56:45 +0900

    Revert "pandad: fall back to GPIO to enter DFU"
    This reverts commit 1141523be73cc0ca9ca78189f4ffaab1a35d2ec6.

--- selfdrive/hardware/tici/pins.py
+++ selfdrive/hardware/tici/pins.py
@@ -1,10 +1,8 @@
 # TODO: these are also defined in a header
-
 # GPIO pin definitions
-class GPIO:
-  HUB_RST_N = 30
-  UBLOX_RST_N = 32
-  UBLOX_SAFEBOOT_N = 33
-  UBLOX_PWR_EN = 34
-  STM_RST_N = 124
-  STM_BOOT0 = 134
+GPIO_HUB_RST_N = 30
+GPIO_UBLOX_RST_N = 32
+GPIO_UBLOX_SAFEBOOT_N = 33
+GPIO_UBLOX_PWR_EN = 34
+GPIO_STM_RST_N = 124
+GPIO_STM_BOOT0 = 134
