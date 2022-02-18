8193ce8acad9563c4fa9e8c3e6f77bc9e5a61ee5
Shane Smiskol <shane@smiskol.com>
2022-02-18 00:45:00 -0800

    Longitudinal control: interpolate longitudinal plan (#23787)
    * interpolate longitudinal actuator delay
    
    rename
    
    * formatting
    
    * interpolate v_target most importantly!
    
    * fix interpolation and rename
    
    * nicer setup
    
    * left in from testing
    
    * update refs

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -59,24 +59,24 @@ class LongControl():
     self.pid.reset()
     self.v_pid = v_pid
 
-  def update(self, active, CS, CP, long_plan, accel_limits, radarState):
+  def update(self, active, CS, CP, long_plan, accel_limits, t_since_plan, radarState):
     """Update longitudinal control. This updates the state machine and runs a PID loop"""
     # Interp control trajectory
-    # TODO estimate car specific lag, use .15s for now
     speeds = long_plan.speeds
-    if len(speeds) == CONTROL_N:
-
+    if len(speeds) == CONTROL_N:      
       longitudinalActuatorDelayLowerBound = ntune_scc_get('longitudinalActuatorDelayLowerBound')
       longitudinalActuatorDelayUpperBound = ntune_scc_get('longitudinalActuatorDelayUpperBound')
+            
+      v_target = interp(t_since_plan, T_IDXS[:CONTROL_N], speeds)
+      a_target = interp(t_since_plan, T_IDXS[:CONTROL_N], long_plan.accels)
 
-      v_target_lower = interp(longitudinalActuatorDelayLowerBound, T_IDXS[:CONTROL_N], speeds)
-      a_target_lower = 2 * (v_target_lower - speeds[0])/longitudinalActuatorDelayLowerBound - long_plan.accels[0]
+      v_target_lower = interp(longitudinalActuatorDelayLowerBound + t_since_plan, T_IDXS[:CONTROL_N], speeds)
+      a_target_lower = 2 * (v_target_lower - v_target) / longitudinalActuatorDelayLowerBound - a_target
 
-      v_target_upper = interp(longitudinalActuatorDelayUpperBound, T_IDXS[:CONTROL_N], speeds)
-      a_target_upper = 2 * (v_target_upper - speeds[0])/longitudinalActuatorDelayUpperBound - long_plan.accels[0]
+      v_target_upper = interp(longitudinalActuatorDelayUpperBound + t_since_plan, T_IDXS[:CONTROL_N], speeds)
+      a_target_upper = 2 * (v_target_upper - v_target) / longitudinalActuatorDelayUpperBound - a_target
       a_target = min(a_target_lower, a_target_upper)
 
-      v_target = speeds[0]
       v_target_future = speeds[-1]
     else:
       v_target = 0.0
