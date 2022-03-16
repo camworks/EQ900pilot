c3445cb6e790bde02914d89000cc08275a7381f5
boxkon <70835190+boxkon@users.noreply.github.com>
2022-03-17 05:19:06 +0900

    Revert "add parkingBrake to carState (#23968)"
    This reverts commit d638c6e4ba194d0fd6070b5b7a0ba087ec9e3a38.

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -143,8 +143,6 @@ class CarState(CarStateBase):
     ret.brake = 0
     ret.brakePressed = cp.vl["TCS13"]["DriverBraking"] != 0
     ret.brakeHoldActive = cp.vl["TCS15"]["AVH_LAMP"] == 2  # 0 OFF, 1 ERROR, 2 ACTIVE, 3 READY
-    ret.parkingBrake = cp.vl["TCS13"]["PBRAKE_ACT"] == 1
-    #ret.parkingBrake = cp.vl["CGW1"]["CF_Gway_ParkBrakeSw"]
 
     # TODO: Check this
     ret.brakeLights = bool(cp.vl["TCS13"]["BrakeLight"] or ret.brakePressed)
@@ -200,6 +198,7 @@ class CarState(CarStateBase):
     self.scc12 = cp_scc.vl["SCC12"]
     self.mdps12 = cp_mdps.vl["MDPS12"]
     self.hda_mfc = cp_cam.vl["LFAHDA_MFC"]    
+    self.park_brake = cp.vl["CGW1"]["CF_Gway_ParkBrakeSw"]
     self.steer_state = cp_mdps.vl["MDPS12"]["CF_Mdps_ToiActive"] #0 NOT ACTIVE, 1 ACTIVE
     self.cruise_unavail_cnt += 1 if cp.vl["TCS13"]["CF_VSM_Avail"] != 1 and cp.vl["TCS13"]["ACCEnable"] != 0 else -self.cruise_unavail_cnt
     self.cruise_unavail = self.cruise_unavail_cnt > 100
