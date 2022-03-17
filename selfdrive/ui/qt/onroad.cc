ba05c4c23e8a3eea5f3d542948c5c200e331ecd8
Igor Biletskyy <briskspirit@users.noreply.github.com>
2022-03-17 13:17:33 -0700

    onroad UI: show negative speed (#23987)
    * show negative speed
    
    * remove ()
    
    * fix s->sm

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -641,7 +641,8 @@ void NvgWindow::drawMaxSpeed(QPainter &p) {
 void NvgWindow::drawSpeed(QPainter &p) {
   UIState *s = uiState();
   const SubMaster &sm = *(s->sm);
-  float cur_speed = std::max(0.0, sm["carState"].getCarState().getCluSpeedMs() * (s->scene.is_metric ? MS_TO_KPH : MS_TO_MPH));
+  float cur_speed = sm["carState"].getCarState().getCluSpeedMs() * (s->scene.is_metric ? MS_TO_KPH : MS_TO_MPH);  
+  //float cur_speed = std::max(0.0, sm["carState"].getCarState().getCluSpeedMs() * (s->scene.is_metric ? MS_TO_KPH : MS_TO_MPH));
   auto car_state = sm["carState"].getCarState();
   float accel = car_state.getAEgo();
 
