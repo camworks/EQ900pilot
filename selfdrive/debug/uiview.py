24fd4de3f85b5ea478bcc166f6791084163c30f5
Shane Smiskol <shane@smiskol.com>
2022-04-14 16:37:09 -0700

    uiview: show model (#24210)
    * assume not car when using uiview
    
    * write carparams

--- selfdrive/debug/uiview.py
+++ selfdrive/debug/uiview.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 import time
-from cereal import messaging, log
-from selfdrive.hardware import HARDWARE
+
+from cereal import car, log, messaging
+from common.params import Params
 from selfdrive.manager.process_config import managed_processes
+from selfdrive.hardware import HARDWARE
 
 if __name__ == "__main__":
-  procs = ['camerad', 'ui', 'modeld', 'calibrationd']
+  CP = car.CarParams(notCar=True)
+  Params().put("CarParams", CP.to_bytes())
 
   HARDWARE.set_power_save(False)
 
+  procs = ['camerad', 'ui', 'modeld', 'calibrationd']
   for p in procs:
     managed_processes[p].start()
   
