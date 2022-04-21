2c5d739bc45be6eb9bf749aa01f9bc40f85dccf5
boxkon <70835190+boxkon@users.noreply.github.com>
2022-04-21 14:14:57 +0900

    fix auto resume

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -216,7 +216,7 @@ class CarController:
         self.resume_wait_timer -= 1
 
       elif abs(CS.lead_distance - self.last_lead_distance) > 0.1:
-        can_sends.append(create_clu11(self.packer, CS.scc_bus, CS.clu11, Buttons.RES_ACCEL, clu11_speed))
+        can_sends.append(create_clu11(self.packer, CS.scc_bus, CS.clu11, Buttons.RES_ACCEL, clu11_speed, speed))
         self.resume_cnt += 1
 
         if self.resume_cnt >= randint(6, 8):
