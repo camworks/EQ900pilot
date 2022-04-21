32c72c6b109f6f82ada37a6ba8c49ea9f325f61d
boxkon <70835190+boxkon@users.noreply.github.com>
2022-04-22 08:29:46 +0900

    Revert "remove lat hybrid"
    This reverts commit efdb7e36ad5dcbbb3239fba05432d46847b65f9a.

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -783,7 +783,7 @@ LateralControl::LateralControl(QWidget* parent): QWidget(parent) {
   QScroller::grabGesture(list->viewport(), QScroller::LeftMouseButtonGesture);
   list->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
 
-  QStringList items = {"TORQUE", "LQR", "INDI"};
+  QStringList items = {"TORQUE", "LQR", "INDI", "HYBRID"};
   list->addItems(items);
   list->setCurrentRow(0);
 
