ba3f0b694cb67cc576ce5a7d0bbb8082d633f622
neokii <neokii@gmail.com>
2022-04-25 10:55:40 +0900

    update min/max for torque

--- selfdrive/ntune.py
+++ selfdrive/ntune.py
@@ -219,7 +219,7 @@ class nTune():
 
     if self.checkValue("useSteeringAngle", 0., 1., 1.):
       updated = True
-    if self.checkValue("maxLatAccel", 1.0, 4.0, 2.8):
+    if self.checkValue("maxLatAccel", 0.5, 5.0, 2.8):
       updated = True
     if self.checkValue("friction", 0.0, 0.2, 0.005):
       updated = True
