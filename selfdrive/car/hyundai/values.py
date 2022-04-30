20388b98c0c2a4c98e6a916defc14ab619908abb
boxkon <70835190+boxkon@users.noreply.github.com>
2022-04-30 23:57:44 +0900

    fix for torque

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -15,11 +15,11 @@ class CarControllerParams:
   def __init__(self, CP):
     self.STEER_MAX = 409   # 409 is the max, 255 is stock
     if CP.lateralTuning == 'torque':
-      self.STEER_DELTA_UP = 3
-      self.STEER_DELTA_DOWN = 7
-    else:
       self.STEER_DELTA_UP = 4
       self.STEER_DELTA_DOWN = 6
+    else:
+      self.STEER_DELTA_UP = 3
+      self.STEER_DELTA_DOWN = 5
     self.STEER_DRIVER_ALLOWANCE = 50
     self.STEER_DRIVER_MULTIPLIER = 2
     self.STEER_DRIVER_FACTOR = 1
